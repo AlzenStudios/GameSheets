668ea5ec46f1d82be6c3a10b1241cb7d235f1e47
mvoud <whaframe@gmail.com>
2025-02-03 10:42:18 +0100

    first

--- progit.sh
+++ progit.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
-
-if [ "$1" == "--init" ]; then
-    if [ -z "$2" ]; then
-        echo "Error: No repository URL provided."
-        exit 1
-    fi
-    git init
-    git remote add origin "$2"
-    echo "Initialized Git repository and added remote: $2"
-    exit 0
-fi
-
 if [ "$1" == "--push" ]; then
     if [ -z "$2" ]; then
         echo "Error: No files specified to add."
@@ -22,14 +9,10 @@ if [ "$1" == "--push" ]; then
         exit 1
     fi
 
+    git pull --rebase origin main
     git add $2
     git commit -m "$4"
     git push origin main
     echo "Changes pushed to main branch."
     exit 0
 fi
-
-echo "Usage:"
-echo "  ./progit.sh --init <git_repo>"
-echo "  ./progit.sh --push <files_to_add> --comment \"yourComment\""
-exit 1
